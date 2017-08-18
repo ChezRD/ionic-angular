f25e2fd375968b8310f89f7da8f1bd265df86aa2
Chez <chezrd@gmail.com>
2017-08-18 14:54:28 +0300

    scrollTop

--- es2015/components/virtual-scroll/virtual-scroll.js
+++ es2015/components/virtual-scroll/virtual-scroll.js
@@ -635,7 +635,7 @@ export class VirtualScroll {
      */
     scrollUpdate(ev) {
         // set the scroll top from the scroll event
-        this._data.scrollTop = ev.scrollTop;
+        this._data.scrollTop = ev ? ev.scrollTop : 0;
         // there is a queue system so that we can
         // spread out the work over multiple frames
         const /** @type {?} */ queue = this._queue;
--- umd/components/virtual-scroll/virtual-scroll.js
+++ umd/components/virtual-scroll/virtual-scroll.js
@@ -665,7 +665,7 @@
         VirtualScroll.prototype.scrollUpdate = function (ev) {
             var _this = this;
             // set the scroll top from the scroll event
-            this._data.scrollTop = ev.scrollTop;
+            this._data.scrollTop = ev ? ev.scrollTop : 0;
             // there is a queue system so that we can
             // spread out the work over multiple frames
             var /** @type {?} */ queue = this._queue;
